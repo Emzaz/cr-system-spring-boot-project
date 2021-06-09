f9215ce033142ecaca9e74f2dfc16c4f7c3f01ac
emzaz <emzazahmednrl@gmail.com>
2021-06-09 22:30:35 +0600

    Home page

---
+++ src/main/java/com/emzaz/crsystemspringboot/CrSystemSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.emzaz.crsystemspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrSystemSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrSystemSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emzaz/crsystemspringboot/CrSystemSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emzaz.crsystemspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrSystemSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
